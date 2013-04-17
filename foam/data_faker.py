b2dd8aaf31f79b9501941b28bf4ae5bf4237971b
rossfadely <rossfadely@gmail.com>
2013-04-17 17:46:01 -0400

    fake data tweaks

--- foam/data_faker.py
+++ foam/data_faker.py
@@ -12,7 +12,7 @@ def create_spectra(Nspec,sed1,sed2):
     sed2[None,:] * (1. - frac[:,None])
     return spec
 
-def default_fake(N):
+def default_fake(N,noise_level=3e-4,rescale=0.25,seed=None):
     data = np.loadtxt('../seds/Ell1_A_0.sed')
     lam1 = data[:,0]
     sed1 = data[:,1]
@@ -21,14 +21,22 @@ def default_fake(N):
     lam2 = data[:,0]
     sed2 = data[:,1]
 
-    noise_level = 3e-4 # hand tuned foo
-
     assert np.sum(lam1-lam2)==0.0, 'Same wavelength grid'
+
+    if seed!=None:
+        np.random.seed(seed)
 
     spectra = create_spectra(N,sed1,sed2)
     noise = noise_level * np.random.randn(spectra.shape[0],
                                           spectra.shape[1])
-    return lam1,spectra+noise,noise_level
+    jitter = noise_level * np.random.randn(spectra.shape[0],
+                                          spectra.shape[1])
+    factor   = noise_level / rescale
+    spectra /= factor
+    noise   /= factor
+    jitter  /= factor
+    return lam1,spectra,spectra+noise+jitter,factor
+
 
 if __name__=='__main__':
     N = 10
