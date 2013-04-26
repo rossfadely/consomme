b07a2dd4f8fd69da93a9970ac323f937a06c7b25
rossfadely <rossfadely@gmail.com>
2013-04-26 17:20:05 -0400

    ortho rotations

--- consomme/rotate_factor.py
+++ consomme/rotate_factor.py
@@ -1,17 +1,22 @@
 import numpy as np
 
-def varimax(lam, eps = 1e-6, itermax=100):
+def ortho_rotation(lam, method = 'varimax',gamma = None,
+                   eps = 1e-6, itermax=100):
     """
-    Return Varimax rotation matrix
+    Return orthogal rotation matrix
 
-    TODO: - what is a good eps?
+    TODO: - other types beyond 
     """
+    if gamma==None:
+        if (method=='varimax'): gamma = 1.0
+        if (method=='quartimax'): gamma = 0.0
+    
     nrow, ncol = lam.shape
     R = np.eye(ncol)
     var = 0
     for i in range(itermax):
         lam_rot = np.dot(lam, R)
-        tmp = np.diag(np.sum(lam_rot ** 2, axis=0)) / nrow
+        tmp = np.diag(np.sum(lam_rot ** 2, axis=0)) / nrow * gamma
         u, s, v = np.linalg.svd(np.dot(lam.T, lam_rot ** 3 - np.dot(lam_rot, tmp)))
         R = np.dot(u, v)
         var_new = np.sum(s)
