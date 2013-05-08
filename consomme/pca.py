6ed354ba8e2dd48b281bfd224aced2007198452f
rossfadely <rossfadely@gmail.com>
2013-05-08 14:08:06 -0400

    bringing pca up to date

--- consomme/pca.py
+++ consomme/pca.py
@@ -3,10 +3,11 @@ import numpy as np
 
 def svd_pca(data):
     """
-    Run PCA using a singular value decomposition.
+    PCA using a singular value decomposition.
     
     `data` is a numpy array, n_samples by n_dimensions
     """
     U, S, eigvec = np.linalg.svd(data)
     eigval = S ** 2. / (data.shape[0] - 1.)
     return eigvec, eigval
+
